e3ed40b4d6343cf4f05eaba166d0072746e38675
Gimhan Rodrigo <gimhanrg@gmail.com>
2022-06-23 12:35:07 +0530

    changes made

--- lib/api/meeting_api.dart
+++ lib/api/meeting_api.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-String MEETING_API_URL = "http://192.168.1.6:4000/api/meeting"; //TODO check url
 var client = http.Client();
 
 Future<http.Response?> startMeeting() async {
@@ -12,8 +12,10 @@ Future<http.Response?> startMeeting() async {
   var token = preferences.getString("userToken");
   //var isLoggedIn = await checkAuth();
 
-  var response = await client.post(Uri.parse('$MEETING_API_URL/start'),
-      headers: requestHeaders, body: jsonEncode({token: token}));
+  var response = await client.post(
+      Uri.parse('${dotenv.env['NODE_API_URL']}/start'),
+      headers: requestHeaders,
+      body: jsonEncode({'token': token}));
 
   if (response.statusCode == 200) {
     return response;
@@ -23,8 +25,8 @@ Future<http.Response?> startMeeting() async {
 }
 
 Future<http.Response> joinMeeting(String meetingId) async {
-  var response =
-      await http.get(Uri.parse('$MEETING_API_URL/join?meetingId=$meetingId'));
+  var response = await http.get(
+      Uri.parse('${dotenv.env['NODE_API_URL']}/join?meetingId=$meetingId'));
 
   if (response.statusCode >= 200 && response.statusCode < 400) {
     return response;
